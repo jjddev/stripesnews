f830d54ccb1a71b5d79ea471a5a04ade6e55ab0b
juliano <julianojd@ymail.com>
2020-06-30 22:55:05 -0300

    Initial Commit

---
+++ stripes news/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stripes news
+//
+//  Created by juliano jose dziadzio on 30/06/20.
+//  Copyright © 2020 juliano jose dziadzio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
